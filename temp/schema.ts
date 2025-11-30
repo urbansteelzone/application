8f3b1ff4abf41b5800f361c2bce3fdef9b428a97
builtbysuraj <surajgupta3553@gmail.com>
2025-11-30 18:08:11 +0530

    Remove temp folder

--- temp/schema.ts
+++
@@ -1,7 +0,0 @@
-import { pgTable, serial, timestamp, varchar } from 'drizzle-orm/pg-core'
-
-export const todos = pgTable('todo', {
-  id: serial('id').primaryKey(),
-  title: varchar('title').notNull(),
-  createdAt: timestamp('created_at').defaultNow(),
-})
